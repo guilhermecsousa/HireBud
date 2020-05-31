67fbe06945e34522aa0051804a891cce9acc5085
Guilherme Sousa <guilhermesousa@ua.pt>
2020-05-31 11:15:15 +0100

    final

--- hirebud/src/screens/service.js
+++ hirebud/src/screens/service.js
@@ -5,6 +5,7 @@ class Service extends Component {
     constructor(props) {
       super(props);
       this.statsRef = React.createRef()
+      this.statsRef2 = React.createRef()
       this.state = {
         loading: true,
         data: null,
@@ -46,6 +47,12 @@ class Service extends Component {
             behavior: 'smooth'    
         })
     }
+    handleScrollToStats2 = () => {
+        window.scrollTo({
+            top: this.statsRef2.current.offsetTop,
+            behavior: 'smooth'    
+        })
+    }
 
     render(){
         if(this.state.loading) return(
@@ -79,76 +86,76 @@ class Service extends Component {
             </header>
             <section class="blog-section spad">
                 <div class="container">
-                    <div class="blog-item feature-blog;">
-                    <div class="container emp-profile">
-            <form method="post">
-                <div class="row">
-                    <div class="profile-img">
-                        <img src={this.state.data.image} style={{minHeight:200, maxHeight:200, minWidth:300, maxWidth:300}}/>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="row">
-                            <div class="profile-head">
-                                <h5>{this.state.data.name}</h5>
-                                <h6>{this.state.data.service}</h6>
-                                <p class="proile-rating">Avaliação : <span>{this.state.data.evaluation}</span></p>
-                                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="" role="tab" aria-controls="home" aria-selected="true">Sobre</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="profile-tab" onClick={this.handleScrollToStats} data-toggle="tab" href="#comments" role="tab" aria-controls="profile" aria-selected="false">Comentários</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            {this.state.cookie==null?
-                                <div class="dropdown" style={{marginLeft:320}}>  
-                                    <button type="button" class="btn btn-info btn-circle-xl btn-lg" style={{width: 150, marginTop:25, marginLeft:100}}><span><i class="fa fa-phone"></i><a class="txt3"> Contacto</a></span></button>
-                                    <div class="dropdown-content">
-                                        <p><i class="fa fa-warning"></i> Necessita iniciar sessão para consultar os dados de contacto</p>
-                                    </div>    
+                    <div class="blog-item feature-blog;">    
+                        <div class="container emp-profile">
+                            <form method="post">
+                                <div class="row">
+                                    <div class="profile-img">
+                                        <img src={this.state.data.image} style={{minHeight:200, maxHeight:200, minWidth:300, maxWidth:300}}/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="row">
+                                            <div class="profile-head">
+                                                <h5>{this.state.data.name}</h5>
+                                                <h6>{this.state.data.service}</h6>
+                                                <p class="proile-rating">Avaliação : <span>{this.state.data.evaluation}</span></p>
+                                                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="" role="tab" aria-controls="home" aria-selected="true">Sobre</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" id="profile-tab" onClick={this.handleScrollToStats} data-toggle="tab" href="#comments" role="tab" aria-controls="profile" aria-selected="false">Comentários</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            {this.state.cookie==null?
+                                                <div class="dropdown" style={{marginLeft:320}}>  
+                                                    <button type="button" class="btn btn-info btn-circle-xl btn-lg" style={{width: 150, marginTop:25, marginLeft:100}}><span><i class="fa fa-phone"></i><a class="txt3"> Contacto</a></span></button>
+                                                    <div class="dropdown-content">
+                                                        <p><i class="fa fa-warning"></i> Necessita iniciar sessão para consultar os dados de contacto</p>
+                                                    </div>    
+                                                </div>
+                                                :
+                                                <div class="dropdown" style={{marginLeft:320}}>  
+                                                    <button type="button" class="btn btn-info btn-circle-xl btn-lg" style={{marginTop:25, marginLeft:180}}><i class="fa fa-phone"></i></button>
+                                                    <div class="dropdown-content">
+                                                        <p><i class="fa fa-mobile-phone"></i> {this.state.data.contact}</p>
+                                                    </div>    
+                                                </div>
+                                                }
+                                        </div>
+                                    </div>
                                 </div>
-                                :
-                                <div class="dropdown" style={{marginLeft:320}}>  
-                                    <button type="button" class="btn btn-info btn-circle-xl btn-lg" style={{marginTop:25, marginLeft:180}}><i class="fa fa-phone"></i></button>
-                                    <div class="dropdown-content">
-                                        <p><i class="fa fa-mobile-phone"></i> {this.state.data.contact}</p>
-                                    </div>    
+                                <div class="row">
+                                    <div class="col-md-7" style={{marginLeft:480}}>
+                                        <div class="tab-content profile-tab" id="myTabContent">
+                                            <div class="tab-pane fade show active"  role="tabpanel" aria-labelledby="home-tab">
+                                                <div class="row">
+                                                    <div class="col-md-6"><label>Área</label></div>
+                                                    <div class="col-md-6"><p>{this.state.data.area}</p></div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-6"><label>Preço</label></div>
+                                                    <div class="col-md-6"><p>{this.state.data.price}</p></div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-6"><label>Estado</label></div>
+                                                    <div class="col-md-6"><p>{this.state.data.state}</p></div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-6"><label>Observações</label></div>
+                                                    <div class="col-md-6"><p>{this.state.data.obs}</p></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                }
-                                
+                            </form>  
+                            <button class="buttonreg btndown" style={{marginLeft:-80}} onClick={this.handleScrollToStats2}><span><i class="fa fa-arrow-down"></i> Anúncios semelhantes </span></button>         
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-7" style={{marginLeft:480}}>
-                        <div class="tab-content profile-tab" id="myTabContent">
-                            <div class="tab-pane fade show active"  role="tabpanel" aria-labelledby="home-tab">
-                                <div class="row">
-                                    <div class="col-md-6"><label>Área</label></div>
-                                    <div class="col-md-6"><p>{this.state.data.area}</p></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6"><label>Preço</label></div>
-                                    <div class="col-md-6"><p>{this.state.data.price}</p></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6"><label>Estado</label></div>
-                                    <div class="col-md-6"><p>{this.state.data.state}</p></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6"><label>Observações</label></div>
-                                    <div class="col-md-6"><p>{this.state.data.obs}</p></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>           
-        </div>
-    </div>
+                     </div>
                 <div ref={this.statsRef}></div>
-                <div class="row bootstrap snippets" style={{marginTop:200}}>
+                <div class="row bootstrap snippets" style={{marginTop:150}}>
                         <div class="col-md-6 col-md-offset-2 col-sm-12">
                             <div class="comment-wrapper" style={{width:900, margin:0}}>
                                 <div class="panel panel-info">
@@ -199,15 +206,18 @@ class Service extends Component {
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="section-title-dest" style={{marginTop:200}}><h6>Serviços de <b>{this.state.data.service}</b> semelhantes:</h6></div>
+                    </div>      
+                    <div style={{marginTop:200}} ref={this.statsRef2}></div>   
+                    <br></br>  <br></br>              
+                    <div class="section-title-dest" ><h6>Serviços de <b>{this.state.data.service}</b> semelhantes:</h6></div>
                     <div class="row">
                         {this.state.data2.map(
                             user=>{
                                 return(
                                     <div class="card" style={{marginRight:28, marginBottom:30}}>
-                                        <img class="card-img-top" src={user.image} style={{width:350, minHeight:250, maxHeight:250}}/>
+                                        <a href={"/service?id="+user.id+"&service="+user.service}>
+                                            <img class="card-img-top" src={user.image} style={{width:350, minHeight:250, maxHeight:250}}/>
+                                        </a>
                                         <div class="card-body">
                                             <div class="row">
                                                 <div class="col">
@@ -229,6 +239,7 @@ class Service extends Component {
                         }   
                     </div>
                 </div>
+                
             </section>
                     
             <footer class="footer-section">
